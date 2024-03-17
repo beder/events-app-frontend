ed0774f950587630be34358511544416dd4bd94a
Beder Acosta Borges <bedermail@yahoo.com>
2024-03-17 15:56:32 +0100

    Update GitHub link in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ export function Footer() {
         <div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
           <div className="flex gap-x-6">
             <Link
-              href="https://github.com/beder"
+              href="https://github.com/beder/events-app-frontend"
               className="group"
               aria-label="Evenia on GitHub"
             >
