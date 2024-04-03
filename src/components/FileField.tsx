1dedd170c4436aae7dad9b200cf2d5cff854a837
Beder Acosta Borges <bedermail@yahoo.com>
2024-04-04 01:11:49 +0200

    Update file upload component

--- src/components/FileField.tsx
+++ src/components/FileField.tsx
@@ -1,4 +1,4 @@
-import { PhotoIcon } from '@heroicons/react/20/solid'
+import { CloudArrowUpIcon } from '@heroicons/react/20/solid'
 import { useFormContext } from 'react-hook-form'
 import { useDropzone } from 'react-dropzone'
 import { useCallback, useEffect } from 'react'
@@ -33,44 +33,28 @@ export function FileField({ name }: { name: string }) {
   }, [register, unregister, name])
 
   return (
-    <div>
-      {!!files?.length ? (
-        <div className="mt-4">
-          <span className="block text-sm font-semibold text-gray-600">
-            <div key={files[0].name}>
-              <Image src={URL.createObjectURL(files[0])} alt={files[0].name} width={96} height={96} />
-            </div>
-          </span>
-        </div>
-      ) : (
-        <PhotoIcon
-          className="mx-auto h-12 w-12 text-gray-300"
-          aria-hidden="true"
+    <div className="relative h-full w-full cursor-pointer" {...getRootProps()}>
+      {!!files?.length && (
+        <Image
+          src={URL.createObjectURL(files[0])}
+          alt={files[0].name}
+          className="absolute inset-0 h-full w-full object-cover opacity-50"
+          width={576}
+          height={384}
         />
       )}
-      <label
-        htmlFor={name}
-        className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-      >
-        <span>Upload a file</span>
-      </label>
-      <div
-        className="mt-4 flex text-sm leading-6 text-gray-600"
-        {...getRootProps()}
-      >
-        <input
-          type="file"
-          className="sr-only"
-          {...register(name)}
-          {...getInputProps()}
-        />
-        <p className="pl-1">
-          {isDragActive ? 'Drop the file here...' : 'Or drag and drop'}
-        </p>
-      </div>
-      <p className="text-xs leading-5 text-gray-600">
-        PNG, JPG, GIF up to 10MB
-      </p>
+      <CloudArrowUpIcon
+        className={`${
+          isDragActive ? ' text-gray-600' : ' text-gray-300'
+        } absolute inset-0 m-auto h-12 w-12 items-center justify-center`}
+        aria-hidden="true"
+      />
+      <input
+        type="file"
+        className="sr-only"
+        {...register(name)}
+        {...getInputProps()}
+      />
     </div>
   )
 }
